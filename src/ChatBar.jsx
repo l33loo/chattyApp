051185aabfb48399e7f477b11f5608887303df96
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-19 02:49:40 +0000

    Add ability for user to change username

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 export default class ChatBar extends Component {
   constructor({ props }) {
     super({ props });
-    this.state = { text: "" };
+    this.state = { text: "", username: "" };
 
     this.onContent = this.onContent.bind(this);
-    this.onEnter = this.onEnter.bind(this);
+    this.onEnterText = this.onEnterText.bind(this);
+    this.onEnterUsername = this.onEnterUsername.bind(this);
   }
 
   // changeText = (event) => {
@@ -20,7 +21,18 @@ export default class ChatBar extends Component {
     // console.log("WOOHOO: ", this.state);
   }
 
-  onEnter(event) {
+  onEnterText(event) {
+    const state = {};
+    if(event.key === "Enter") {
+      // console.log("STATE before: ", this.state);
+      this.props.onNewPost(this.state.text);
+      state.text = "";
+      // console.log("STATE AFTER: ", this.state);
+    }
+    this.setState(state);
+  }
+
+  onEnterUsername(event) {
     const state = {};
     if(event.key === "Enter") {
       // console.log("STATE before: ", this.state);
@@ -39,10 +51,12 @@ export default class ChatBar extends Component {
       // this.props.sendMessage(this.state.text);
       this.setState({ text: '' });
     };
+
+    const checkUser = this.props.currentUser ? this.props.currentUser : this.state.username
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" defaultValue={this.props.currentUser} placeholder="Your Name (Optional)"  />
-        <input className="chatbar-message" value={ this.state.text } onChange={this.onContent} placeholder="Type a message and hit ENTER" onKeyPress={this.onEnter} />
+        <input className="chatbar-username" defaultValue={ checkUser } placeholder="Your Name (Optional) and hit ENTER" onKeyPress={ this.onEnterText } />
+        <input className="chatbar-message" value={ this.state.text } onChange={this.onContent} placeholder="Type a message and hit ENTER" onKeyPress={ this.onEnterUsername } />
       </footer>);
   }
 }
