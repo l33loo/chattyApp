8dbfe29357ce8fbd5ce0de324d0b00538f6134d3
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-19 23:43:04 +0000

    Differentiate between chat messages and notifications

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-const Message = ({ message }) => (
-
-  <div className="message">
-    <span className="message-username">{message.username}</span>
-    <span className="message-content">{message.content}</span>
-  </div>);
+const Message = ({ message }) => {
+  switch(message.type) {
+    case "incomingMessage":
+    const username = message.username? message.username : "anonymous";
+      return <div className="message">
+        <span className="message-username">{ username }</span>
+        <span className="message-content">{ message.content }</span>
+      </div>
+      break;
+    case "incomingNotification":
+      return <div className="notification">
+        <span className="notification-content">{ message.content }</span>
+      </div>
+      break;
+  }
+};
 
 export default Message;
