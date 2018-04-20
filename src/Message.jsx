668538fdac91f1f4750982e078cb4439f796d7b6
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-20 03:23:16 +0000

    Convert image links into embedded images; remove anonymous from username default value

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const Message = ({ message }) => {
-  console.log("COLOR: ", message.color)
+  const username = message.username? message.username : "anonymous";
   const usernameColor = {color: message.color};
+  const last3digitsText = message.content.slice(message.content.length - 3);
+  console.log("LAST 3 DIGITS: ", typeof last3digitsText);
+  if (last3digitsText === "jpg" || last3digitsText === "png" || last3digitsText === "gif") {
+    return <div className="message">
+        <span style={ usernameColor } className="message-username">{ username }</span>
+        <span className="message-content"><img src={ message.content }/></span>
+      </div>;
+  } else {
   switch(message.type) {
     case "incomingMessage":
-    const username = message.username? message.username : "anonymous";
       return <div className="message">
         <span style={ usernameColor } className="message-username">{ username }</span>
         <span className="message-content">{ message.content }</span>
@@ -17,6 +24,7 @@ const Message = ({ message }) => {
       </div>
       break;
   }
+}
 };
 
 export default Message;
