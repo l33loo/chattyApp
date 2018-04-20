3c13adc0afcb49088b3f534c47e2b3b8c5ff4899
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-20 03:46:55 +0000

    Clean code and add mentor's suggestions as comments

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 
 const Message = ({ message }) => {
-  const username = message.username? message.username : "anonymous";
+  const username = message.username ? message.username : "anonymous";
   const usernameColor = {color: message.color};
+
+  //create isImage fxn
   const last3digitsText = message.content.slice(message.content.length - 3);
-  console.log("LAST 3 DIGITS: ", typeof last3digitsText);
   if (last3digitsText === "jpg" || last3digitsText === "png" || last3digitsText === "gif") {
+
+    //convert to fxn (to put at top)
     return <div className="message">
         <span style={ usernameColor } className="message-username">{ username }</span>
         <span className="message-content"><img src={ message.content }/></span>
       </div>;
   } else {
   switch(message.type) {
+
+    //convert to fxn
     case "incomingMessage":
       return <div className="message">
         <span style={ usernameColor } className="message-username">{ username }</span>
         <span className="message-content">{ message.content }</span>
       </div>
       break;
+
+    //convert to function
     case "incomingNotification":
       return <div className="notification">
         <span className="notification-content">{ message.content }</span>
