6d46ed19c8aad3994ffb0773647999caa484ab83
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-20 02:31:20 +0000

    Add username's own color to message

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -12,6 +12,16 @@ const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  let usernameColor = "";
+  if (wss.clients.size % 4 === 0) {
+    usernameColor = "#961092";
+  } else if (wss.clients.size % 3 === 0) {
+    usernameColor = "#101496";
+  } else if (wss.clients.size % 2 === 0) {
+    usernameColor = "#1f9610";
+  } else if (wss.clients.size % 1 === 0)  {
+    usernameColor = "#a51c29";
+  }
   const numberConnectedMsg = { type: 'changeConnection', connected: wss.clients.size };
   wss.clients.forEach((client) => {
     client.send(JSON.stringify(numberConnectedMsg));
@@ -22,7 +32,7 @@ wss.on('connection', (ws) => {
         const msgObj = JSON.parse(data);
         if (msgObj.type === "postMessage") {
           const { username, content } = msgObj;
-          const msgObjWithId = { type: 'incomingMessage', id: uuidv4(), username, content }
+          const msgObjWithId = { type: 'incomingMessage', id: uuidv4(), username, content, color: usernameColor }
           client.send(JSON.stringify(msgObjWithId));
           console.log("incomingMessage: ", JSON.stringify(msgObjWithId));
         } else {
