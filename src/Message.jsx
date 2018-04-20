b1c046e6235f081cb2a835e6ecc0cec7eb2eb318
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-20 02:33:20 +0000

    Add inline style for username color

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Message = ({ message }) => {
+  console.log("COLOR: ", message.color)
+  const usernameColor = {color: message.color};
   switch(message.type) {
     case "incomingMessage":
     const username = message.username? message.username : "anonymous";
       return <div className="message">
-        <span className="message-username">{ username }</span>
+        <span style={ usernameColor } className="message-username">{ username }</span>
         <span className="message-content">{ message.content }</span>
       </div>
       break;
