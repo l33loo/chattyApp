34362b42fd393c60fb0a5f25a850100e93ec5ea7
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-20 03:48:26 +0000

    Fix syntax

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import Message from './Message.jsx';
 
 function MessageList ({ messages }) {
-  const messageComponents = messages.map((msg) => (<Message key={ msg.id } message={ msg }/>))
-    return (
-      <main className="messages">
-          {messageComponents}
-      </main>
-    );
+  const messageComponents = messages.map((msg) => (<Message key={ msg.id } message={ msg }/>));
+  return (
+    <main className="messages">
+      { messageComponents }
+    </main>
+  );
 }
 
 export default MessageList;
