c1e57b88f7f2884a1fe0dcdef38ff9703bf40550
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-21 00:22:18 +0000

    Create function that verifies whether a message is a url to a photo; fix indentation; remove comments

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,37 +1,34 @@
 import React from 'react';
 
+const isImage = (message) => {
+  const last3digitsText = message.slice(message.length - 3);
+  return last3digitsText === "jpg" || last3digitsText === "png" || last3digitsText === "gif"
+};
+
 const Message = ({ message }) => {
   const username = message.username ? message.username : "anonymous";
   const usernameColor = {color: message.color};
 
-  //create isImage fxn
-  const last3digitsText = message.content.slice(message.content.length - 3);
-  if (last3digitsText === "jpg" || last3digitsText === "png" || last3digitsText === "gif") {
-
-    //convert to fxn (to put at top)
+  if (isImage(message.content)) {
     return <div className="message">
-        <span style={ usernameColor } className="message-username">{ username }</span>
-        <span className="message-content"><img src={ message.content }/></span>
-      </div>;
+      <span style={ usernameColor } className="message-username">{ username }</span>
+      <span className="message-content"><img src={ message.content }/></span>
+    </div>;
   } else {
-  switch(message.type) {
-
-    //convert to fxn
-    case "incomingMessage":
-      return <div className="message">
-        <span style={ usernameColor } className="message-username">{ username }</span>
-        <span className="message-content">{ message.content }</span>
-      </div>
-      break;
-
-    //convert to function
-    case "incomingNotification":
-      return <div className="notification">
-        <span className="notification-content">{ message.content }</span>
-      </div>
-      break;
+    switch(message.type) {
+      case "incomingMessage":
+        return <div className="message">
+          <span style={ usernameColor } className="message-username">{ username }</span>
+          <span className="message-content">{ message.content }</span>
+        </div>;
+        break;
+      case "incomingNotification":
+        return <div className="notification">
+          <span className="notification-content">{ message.content }</span>
+        </div>;
+        break;
+    }
   }
-}
 };
 
 export default Message;
