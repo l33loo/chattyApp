0e9b3dd5fc9652fccb4804801f1a9af67b1f9bca
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-21 00:51:14 +0000

    Fix color functionality

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -11,17 +11,17 @@ const server = express()
 const wss = new WebSocket.Server({ server });
 
 // assigns colors cyclically
-const assignColorToUsername = () => {
-  if (wss.clients.size % 4 === 0) {
+const assignColorToUsername = (nrClients) => {
+  if (nrClients % 4 === 0) {
     return "#961092";
   }
-  if (wss.clients.size % 3 === 0) {
+  if (nrClients % 3 === 0) {
     return "#101496";
   }
-  if (wss.clients.size % 2 === 0) {
+  if (nrClients % 2 === 0) {
     return "#1f9610";
   }
-  if (wss.clients.size % 1 === 0)  {
+  if (nrClients % 1 === 0)  {
     return "#a51c29";
   }
 };
@@ -29,19 +29,19 @@ const assignColorToUsername = () => {
 wss.on('connection', (ws) => {
   console.log('Client connected');
   const numberConnectedMsg = { type: 'changeConnection', connected: wss.clients.size };
+  const nrClients = wss.clients.size;
 
   wss.clients.forEach((client) => {
     client.send(JSON.stringify(numberConnectedMsg));
   });
 
   ws.on('message', (data) => {
-
     wss.clients.forEach((client) => {
       const msgObj = JSON.parse(data);
       const { username, content } = msgObj;
       const msgObjWithId = { id: uuidv4(), username, content };
       if (msgObj.type === "postMessage") {
-        client.send(JSON.stringify({ type: 'incomingMessage', ...msgObjWithId, color: assignColorToUsername() }));
+        client.send(JSON.stringify({ type: 'incomingMessage', ...msgObjWithId, color: assignColorToUsername(nrClients) }));
       } else {
         client.send(JSON.stringify({ type: 'incomingNotification', ...msgObjWithId }));
         }
